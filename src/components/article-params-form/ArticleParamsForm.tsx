1c222554cc6f164d47cd6d90ca62a2c03b2fb696
Milena Fedorova <Milory88@yandex.ru>
2025-10-31 12:34:40 +0300

    make project

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,100 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState, useEffect  } from 'react';
+import {fontFamilyOptions, ArticleStateType, OptionType, fontColors, fontSizeOptions, backgroundColors, contentWidthArr, defaultArticleState} from '../../constants/articleProps'
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	currentArticleState: ArticleStateType;
+	setCurrentArticleState: (param: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({currentArticleState, setCurrentArticleState}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const asideRef = useRef<HTMLDivElement | null>(null);
+	const [choosenArticleParams, setChoosenArticleParams] = useState<ArticleStateType>(currentArticleState)
+
+	useEffect(() => {
+		function handleClickOutside(event: MouseEvent) {
+		  if (asideRef.current && !asideRef.current.contains(event.target as Node)) {
+			setIsOpen(false);
+		  }
+		}
+		if (isOpen) {
+		  document.addEventListener('mousedown', handleClickOutside);
+		}
+		return () => {
+		  document.removeEventListener('mousedown', handleClickOutside);
+		};
+	  }, [isOpen]);
+
+    function handleOpen() {
+      setIsOpen((prev) => !prev);
+    }
+
+	function handleChange(key: keyof ArticleStateType, value: OptionType) {
+		setChoosenArticleParams({...choosenArticleParams, [key]: value})
+	}
+
+	function handleReset(e: React.FormEvent) {
+		e.preventDefault();
+		setChoosenArticleParams(defaultArticleState);
+		setCurrentArticleState(defaultArticleState)
+	}
+
+	function handleSubmit(e: React.FormEvent) {
+		e.preventDefault();
+		setCurrentArticleState(choosenArticleParams)
+	}
+
+
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={handleOpen} />
+			<aside ref={asideRef} className={`${styles.container} ${isOpen ? styles.container_open : ''}`}>
+				<form className={styles.form} onReset={handleReset} onSubmit={handleSubmit}>
+
+				    <Text size={31} uppercase={true} weight={800}>
+						Задайте параметры
+					</Text>
+
+					<Select selected={choosenArticleParams.fontFamilyOption}
+					options={fontFamilyOptions}
+					title={'Шрифт'}
+					onChange={(option) => handleChange("fontFamilyOption", option)}/>
+
+                    <RadioGroup
+						selected={choosenArticleParams.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => handleChange('fontSizeOption', option)}
+						name='Размер шрифта'
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={choosenArticleParams.fontColor}
+						options={fontColors}
+						onChange={(option) => handleChange('fontColor', option)}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={choosenArticleParams.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => handleChange('backgroundColor', option)}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={choosenArticleParams.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => handleChange('contentWidth', option)}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
